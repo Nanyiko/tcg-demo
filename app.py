1a9e584fa515a152fb272dc4517d1756b63fe38e
Nanyiko <nanabyanyiko@gmail.com>
2026-05-19 11:24:15 +0000

    Testing scanner

--- app.py
+++ app.py
@@ -49,3 +49,8 @@ def init_db():
     if not os.path.exists(db_path):
         with app.app_context():
             db.create_all()
+
+init_db()
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
